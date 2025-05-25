9cd21e91788eff352a156dce66cc5acfe06cafb6
luc1fer007 <arifulhassanjoy02@gmail.com>
2025-05-25 11:15:00 +0600

    Update 66.fibonacci series.c

---
+++ 66.fibonacci series.c
@@ -0,0 +1,45 @@
+//Effective method to do so///
+#include<stdio.h>
+int main()
+{
+    int N,i,p1 = 0 , p2 = 1,temp;
+    scanf("%d",&N);
+    for(i = 1 ; i<= N ; i++)
+    {
+        if(i == 1)
+        {
+            printf("0");
+        }
+        else if(i == 2)
+        {
+            printf(" 1");
+        }
+        else
+        {printf(" %d",p1+p2);
+        temp = p2;
+        p2 = p1 + p2;
+        p1 = temp;
+        }
+
+    }
+    
+}
+
+
+//another method to do so but not effective///
+#include<stdio.h>
+int main()
+{
+    short int N,i,p1 = 0 , p2 = 1,temp;
+    scanf("%hd",&N);
+    printf("%hd %hd ",p1,p2);
+    for(i = 3 ; i<= N ; i++)
+    {
+        printf(" %hd",p1+p2);
+        temp = p2;
+        p2 = p1 + p2;
+        p1 = temp;
+
+    }
+    
+}
